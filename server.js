55b301a1fd2cab401a35af458548fc07673795d7
Alijonov Shukhratbek <shukhratbekalijonov5@gmail.com>
2025-12-25 19:27:30 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data)=> {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1 : Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 
 // 2: Session code 
 
@@ -15,6 +26,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
+app.post("/create-item",(req, res)=> {
+    //TODO:with db here
+});
+
+app.get('/author', (req,res) => {
+    res.render("author", {user: user});
+});
+
 app.get("/", function (req, res) {
     res.render ("harid");
 });
